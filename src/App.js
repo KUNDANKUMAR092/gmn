a28eed0ef0c8ca86993fa1afe3139a72aba0c14d
Kundan Kumar <kundan.kumar@mantralabs.tech>
2023-08-13 00:27:25 +0530

    change doc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 import './App.css';
 import Header from './components/Header/Header';
@@ -8,27 +8,27 @@ import Login from './login/Login';
 
 function App() {
 
-  const [studentList, setStudentList] = useState([]);
-  useEffect(() => {
-    axios.get('http://127.0.0.1:8000/studentapi/')
-      .then(response => {
-        console.log("response==>", response.data);
-        setStudentList(response.data);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-    // axios({
-    //   method: 'get',
-    //   url: 'http://127.0.0.1:8000/studentapi/',
-    //   data: {}
-    // }).then(response => {
-    //   setStudentList(response.data);
-    // })
-    // .catch(error => {
-    //   console.error(error);
-    // });
-  }, []);
+  // const [studentList, setStudentList] = useState([]);
+  // useEffect(() => {
+  //   axios.get('http://127.0.0.1:8000/studentapi/')
+  //     .then(response => {
+  //       console.log("response==>", response.data);
+  //       setStudentList(response.data);
+  //     })
+  //     .catch(error => {
+  //       console.error(error);
+  //     });
+  //   axios({
+  //     method: 'get',
+  //     url: 'http://127.0.0.1:8000/studentapi/',
+  //     data: {}
+  //   }).then(response => {
+  //     setStudentList(response.data);
+  //   })
+  //   .catch(error => {
+  //     console.error(error);
+  //   });
+  // }, []);
 
 
   return (
